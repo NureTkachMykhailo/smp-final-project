26846ec48dbe98339768d9ecdf5d55f6c83f83b4
NureTkachMykhailo <211744814+NureTkachMykhailo@users.noreply.github.com>
2025-06-28 15:11:18 +0300

    Add config files

---
+++ config/database.php
@@ -0,0 +1,20 @@
+<?php
+// config/database.php - PRODUCTION VERSION with your exact details
+return [
+    'host' => 'sql108.infinityfree.com',
+    'database' => 'if0_39340877_handmade_shop',
+    'username' => 'if0_39340877',
+    'password' => 'v7736PIt2mnUle',
+    'charset' => 'utf8mb4'
+];
+
+/*
+Your database is ready! Connection details:
+- Host: sql108.infinityfree.com
+- Database: if0_39340877_handmade_shop  
+- Username: if0_39340877
+- Password: v7736PIt2m01ic (your account password)
+
+The database has been created successfully and is ready for data import.
+*/
+?>  
